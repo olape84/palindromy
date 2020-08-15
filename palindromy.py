db04bae57c3aa6885e2620f936a048f0d17925ff
Aleksandra Przywrzej <przepjurka@gmail.com>
2020-08-15 14:40:42 +0200

    pierwsze podejscie do palindromow

---
+++ palindromy.py
@@ -0,0 +1,10 @@
+def palindromy ():
+    input("Wprowadz tekst, który chcesz sprawdzic czy jest palindromem i wcisnij enter")
+    len(input)
+    if tekst[0] == tekst[len-1]:
+      #  tekst +=1 and len -=1
+        boolean: True
+    else:
+        boolean: False
+    return
+palindromy()
