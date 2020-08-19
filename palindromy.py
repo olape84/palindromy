d5944088ab2d2fda4831cb7792d63cba421cac45
Aleksandra Przywrzej <przepjurka@gmail.com>
2020-08-19 18:08:13 +0200

    funkcja liczaca pary literek w palindromie

--- palindromy.py
+++ palindromy.py
@@ -1,10 +1,10 @@
 def palindromy ():
     input("Wprowadz tekst, który chcesz sprawdzic czy jest palindromem i wcisnij enter")
-    len(input)
-    if tekst[0] == tekst[len-1]:
-      #  tekst +=1 and len -=1
-        boolean: True
-    else:
-        boolean: False
-    return
+    tekst = input()
+    for i in range(len(tekst)):
+        if tekst[i] != tekst[-i-1]:
+            return False
+    return True
 palindromy()
+
+
