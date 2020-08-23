78da39be7b76c5a9e22364aa3001b5fe6c4cc19b
Aleksandra Przywrzej <przepjurka@gmail.com>
2020-08-23 18:28:14 +0200

    ostatnia dzialajaca wersja palindromow

--- palindromy.py
+++ palindromy.py
@@ -1,10 +1,10 @@
-def palindromy ():
-    input("Wprowadz tekst, który chcesz sprawdzic czy jest palindromem i wcisnij enter")
-    tekst = input()
+def palindromy(tekst):
     for i in range(len(tekst)):
-        if tekst[i] != tekst[-i-1]:
+        if not tekst[i] == tekst[-i-1]:
             return False
     return True
-palindromy()
 
-
+if palindromy(input("Wprowadz tekst, który chcesz sprawdzic czy jest palindromem i wcisnij enter")):
+    print('Twoje słowo jest palindromem')
+else:
+    print('Twoje słowo nie jest palindromem')
